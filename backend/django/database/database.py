54bc9b7ccd68b02f7b159e2715a3fa0ddc2885ec
Raphael <raphaelricardo10@gmail.com>
2021-10-12 12:03:31 -0300

    changed folder structure

--- backend/django/database/database.py
+++ backend/django/database/database.py
@@ -10,6 +10,6 @@ class Database:
         uri = "mongodb+srv://cluster0.uargx.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
         client = MongoClient(uri,
                             tls=True,
-                            tlsCertificateKeyFile='/home/raphael/aquarela/backend/X509-cert-6036829648392384800.pem')
+                            tlsCertificateKeyFile='../X509-cert-6036829648392384800.pem')
 
         self.connection = client[dbName]
