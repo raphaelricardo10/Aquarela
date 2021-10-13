9c3c12301b61284c6e3b6a76a1f39680747d95a7
Raphael <raphaelricardo10@gmail.com>
2021-10-13 14:51:23 -0300

    Started the testing phase

--- tests/testRequest.py
+++ tests/testRequest.py
@@ -1,15 +1,17 @@
 import unittest
 import requests
 import json
+import os
 
 class TestRequestMethods(unittest.TestCase):
 
     def test_wronginput(self):
-        with open('./queries/wrongInput.json') as json_file:
+        with open(os.path.dirname(os.path.realpath(__file__)) + "/queries/wrongInput.json") as json_file:
             queries = json.load(json_file)
             url = 'http://127.0.0.1:5000/sales'
 
             for query in queries:
                 with self.subTest(query=query):
                     response = requests.post(url, json=query)
-                    self.assertNotEquals(response.status_code, 200)
+                    print(response.content)
+                    self.assertNotEqual(response.status_code, 200)
