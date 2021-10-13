c09ec65b0e0c4c8f2c565d04288b26668c3213ac
Raphael <raphaelricardo10@gmail.com>
2021-10-12 23:08:59 -0300

    Created basic backend queries

--- backend/flask/routes/sales.py
+++ backend/flask/routes/sales.py
@@ -1,6 +1,58 @@
 from flask.blueprints import Blueprint
+from flask import request, Response
+from bson.json_util import dumps, loads
+from src.database import Database
+from bson.objectid import ObjectId
+from datetime import datetime
 
 sales = Blueprint('sales', __name__)
-@sales.route("/sales")
-def show():
-    return "<p>Hello, World!</p>"
+@sales.route("/sales", methods=['POST'])
+def get():
+    if request.method == 'POST':
+        r = request.json
+
+        try:
+            db = Database('sample_supplies')
+        except Exception:
+            return Response({"Error": "Failed in connecting to the database. Try again later"}, status=500)
+
+        try:
+            if r is None:
+                r = {}
+
+            if not 'data' in r:
+                r['data'] = {}
+
+
+            data = r['data']
+
+            if 'saleDate' in data:
+                date = datetime.strptime(data['saleDate'], '%Y-%m-%d')
+                data['saleDate'] = {
+                        "$gte": datetime(date.year, date.month, date.day, 0, 0, 0),
+                        "$lt": datetime(date.year, date.month, date.day, 23, 59, 59)
+                    }
+
+            if '_id' in data:
+                data['_id'] = ObjectId(data['_id'])
+
+            if 'params' in r:
+                if 'limit' not in r['params']:
+                    r['params']['limit'] = 0
+            else:
+                r['params'] = {}
+                r['params']['limit'] = 0
+
+        except Exception as e:
+            return Response(e, status=400)
+
+        try:
+            response = dumps(db.connection.sales.find(data).limit(r['params']['limit']))
+
+            if response == '[]':
+                raise StopIteration
+
+            return Response(response, status=200)
+
+        except StopIteration:
+            return Response({"Warning": "The requested document was not found in database"}, status=204)
--- backend/flask/src/database.py
+++ backend/flask/src/database.py
@@ -1,15 +1,23 @@
+import os
 from pymongo import MongoClient
 
 class Database:
     def __init__(self, dbName=None) -> None:
         self.connection = None
         if(dbName):
-            self.connect(dbName)
+            try:
+                self.connect(dbName)
+            except:
+                raise
 
     def connect(self, dbName: str) -> None:
         uri = "mongodb+srv://cluster0.uargx.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
-        client = MongoClient(uri,
-                            tls=True,
-                            tlsCertificateKeyFile='../X509-cert-6036829648392384800.pem')
+        certFile = os.path.abspath(__file__ + '/../../../') + '/X509-cert-6036829648392384800.pem'
+        try:
+            client = MongoClient(uri,
+                                tls=True,
+                                tlsCertificateKeyFile=certFile)
+        except:
+            raise ConnectionError
 
         self.connection = client[dbName]
